459abcc39e078a384686d4477edab5e15ad63d8f
rpgeeganage <rpgeeg@gmail.com>
2019-01-19 17:31:26 +0100

    added documentation

--- lib/ifto.ts
+++ lib/ifto.ts
@@ -1,54 +1,160 @@
 import { Context } from 'aws-lambda';
 
+/**
+ * Function signature of the expected output function
+ */
 export type OutputFunction = (...arg: any[]) => void;
+
+/**
+ * Environment variable to indicate monitoring should start
+ */
 export const ENV_VAR_MONITORING_START_NAME = 'ifto_start';
+
+/**
+ * Environment variable to enable to disable monitoring
+ */
 export const ENV_VAR_MONITORING_START_VALUE = 'true';
+
+/**
+ * Environment variable to configure "flushLogsWhenDifferenceLessThanMilliseconds"
+ */
 export const ENV_VAR_FLUSH_LOGS_WHEN_DIFFERENCE_LESS_THAN_MILLISECONDS =
   'ifto_flush_when';
 
+/**
+ * Extended Global variable.
+ * Holds the Ifto instance
+ * @export
+ * @interface ExtendedNodeJsGlobal
+ * @extends {NodeJS.Global}
+ */
 export interface ExtendedNodeJsGlobal extends NodeJS.Global {
   Ifto?: Ifto;
 }
 
-export interface Options {
+/**
+ * Option interface
+ *
+ * @interface Options
+ */
+interface Options {
   flushLogsWhenDifferenceLessThanMilliseconds: number;
   output: OutputFunction;
 }
 
+/**
+ * Default options
+ */
 const defaults: Options = {
   flushLogsWhenDifferenceLessThanMilliseconds: 50,
   output: console.log
 };
 
+/**
+ * If lambda Timeout (Ifto) main class.
+ *
+ * @export
+ * @class Ifto
+ */
 export class Ifto {
-  private readyToCache = false;
+  /**
+   * flag indicates monitoring should enable or not
+   *
+   * @private
+   * @memberof Ifto
+   */
+  private readyMonitor = false;
+
+  /**
+   * Holds the log entries
+   *
+   * @private
+   * @type {string[]}
+   * @memberof Ifto
+   */
   private logEntries: string[] = [];
+
+  /**
+   * This holds minimum value
+   *
+   * @private
+   * @type {number}
+   * @memberof Ifto
+   */
   private flushLogsWhenDifferenceLessThanMilliseconds: number;
+
+  /**
+   * Holds the output function
+   *
+   * @private
+   * @type {OutputFunction}
+   * @memberof Ifto
+   */
   private output: OutputFunction;
+
+  /**
+   * Holds the lambda Context object
+   *
+   * @private
+   * @type {Context}
+   * @memberof Ifto
+   */
   private lambdaContext?: Context;
 
+  /**
+   * Creates an instance of Ifto.
+   * @memberof Ifto
+   */
   constructor() {
     this.flushLogsWhenDifferenceLessThanMilliseconds =
       defaults.flushLogsWhenDifferenceLessThanMilliseconds;
     this.output = defaults.output;
   }
 
-  static getInstance() {
+  /**
+   * Get instance of Ifto
+   *
+   * @static
+   * @returns {Ifto}
+   * @memberof Ifto
+   */
+  static getInstance(): Ifto {
     return new Ifto();
   }
 
+  /**
+   * Add Lambda function context
+   *
+   * @param {Context} lambdaContext
+   * @returns
+   * @memberof Ifto
+   */
   addContext(lambdaContext: Context) {
     this.lambdaContext = lambdaContext;
 
     return this;
   }
 
+  /**
+   * Update output function
+   *
+   * @param {OutputFunction} output
+   * @returns
+   * @memberof Ifto
+   */
   updateOutputFunction(output: OutputFunction) {
     this.output = output;
 
     return this;
   }
 
+  /**
+   * Attach to Global object
+   *
+   * @param {ExtendedNodeJsGlobal} globalObject
+   * @returns
+   * @memberof Ifto
+   */
   attachToGlobal(globalObject: ExtendedNodeJsGlobal) {
     if (!globalObject.Ifto) {
       globalObject.Ifto = this;
@@ -57,9 +163,16 @@ export class Ifto {
     return this;
   }
 
+  /**
+   * Initialize the Ifto process
+   *
+   * @param {NodeJS.Process} currentProcess
+   * @returns
+   * @memberof Ifto
+   */
   init(currentProcess: NodeJS.Process) {
     const envValue = currentProcess.env[ENV_VAR_MONITORING_START_NAME];
-    this.readyToCache = !!(
+    this.readyMonitor = !!(
       envValue &&
       envValue.toLocaleLowerCase() === ENV_VAR_MONITORING_START_VALUE
     );
@@ -82,10 +195,17 @@ export class Ifto {
     return this;
   }
 
+  /**
+   * Start monitoring
+   *
+   * @param {Promise<any>} executor
+   * @returns
+   * @memberof Ifto
+   */
   monitor(executor: Promise<any>) {
     return new Promise((resolve, reject) => {
       let interval: NodeJS.Timeout;
-      if (this.readyToCache && this.lambdaContext) {
+      if (this.readyMonitor && this.lambdaContext) {
         interval = setInterval(() => {
           this.output(this.logEntries.join('\n'));
         }, this.lambdaContext.getRemainingTimeInMillis() - this.flushLogsWhenDifferenceLessThanMilliseconds);
@@ -93,30 +213,53 @@ export class Ifto {
 
       executor
         .then((data: any) => {
-          this.clearLogging(interval);
+          this.monitoringLogging(interval);
 
           return resolve(data);
         })
         .catch((err) => {
-          this.clearLogging(interval);
+          this.monitoringLogging(interval);
 
           return reject(err);
         });
     });
   }
 
+  /**
+   * Log data
+   *
+   * @param {string} entry
+   * @returns
+   * @memberof Ifto
+   */
   log(entry: string) {
     this.logEntries.push(this.getLogEntry(this.logEntries, entry));
 
     return this;
   }
 
-  private clearLogging(interval?: NodeJS.Timeout) {
+  /**
+   * Clear
+   *
+   * @private
+   * @param {NodeJS.Timeout} [interval]
+   * @memberof Ifto
+   */
+  private monitoringLogging(interval?: NodeJS.Timeout) {
     if (interval) {
       clearInterval(interval);
     }
   }
 
+  /**
+   * Get formated log entry
+   *
+   * @private
+   * @param {string[]} logArray
+   * @param {string} entry
+   * @returns
+   * @memberof Ifto
+   */
   private getLogEntry(logArray: string[], entry: string) {
     const date = this.getTime();
     return `${date.y}-${date.mon}-${date.d}T${date.h}:${date.m}:${date.s}.${
@@ -124,6 +267,14 @@ export class Ifto {
     } ${logArray.length}: ${entry}`;
   }
 
+  /**
+   * Get time value
+   *
+   * @private
+   * @returns { y: number, mon: number, d: number, h: number, m: number, s: number, ms: number}
+    }
+   * @memberof Ifto
+   */
   private getTime() {
     const date = new Date();
     return {
--- lib/index.ts
+++ lib/index.ts
@@ -1,11 +1,29 @@
 import { Ifto } from './ifto';
+
+/**
+ * New declaration of the Global variable with Ifto instance
+ */
 declare global {
   const Ifto: Ifto;
 }
+
+/**
+ * Export Ifto class and interface
+ */
 export * from './ifto';
+
+/**
+ * Start Ifto. Attach to global object
+ *
+ * @export
+ * @param {NodeJS.Global} globalObject
+ */
 export function start(globalObject: NodeJS.Global) {
   const ifTo = Ifto.getInstance();
   ifTo.attachToGlobal(globalObject);
 }
 
+/**
+ * Start the Ifto
+*/
 start(global);
